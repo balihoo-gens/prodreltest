e2f8bc066fc50b132eb222052d7d674d6fdd5a2a
gerry ens <gens@balihoo.com>
2014-06-20 15:23:25 -0600

    basic SESAdapter completed

--- src/main/scala/com/balihoo/fulfillment/SESAdapter.scala
+++ src/main/scala/com/balihoo/fulfillment/SESAdapter.scala
@@ -1,8 +1,20 @@
 package com.balihoo.fulfillment
 
+import scala.collection.JavaConverters._
+import scala.collection.mutable.ListBuffer
+
 import com.balihoo.fulfillment.config.PropertiesLoader
 import com.amazonaws.auth.BasicAWSCredentials
 import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceAsyncClient
+import com.amazonaws.services.simpleemail.model.{
+  VerifyEmailAddressRequest,
+  SendEmailRequest,
+  ListIdentitiesRequest,
+  Destination,
+  Message,
+  Body,
+  Content
+}
 
 class SESAdapter(loader: PropertiesLoader) {
   private val accessKey: String = loader.getString("aws.accessKey")
@@ -13,4 +25,26 @@ class SESAdapter(loader: PropertiesLoader) {
 
   val config = loader
 
+  def verifyEmailAddress(address: String) = {
+    var request = new VerifyEmailAddressRequest()
+    request.setEmailAddress(address)
+    client.verifyEmailAddress(request)
+  }
+
+  def listVerifiedEmailAddresses(): List[String] = {
+    var request = new ListIdentitiesRequest()
+    request.setIdentityType("EmailAddress")
+    val result = client.listIdentities(request)
+    result.getIdentities().asScala.toList
+  }
+
+  def sendEmail(from: String, recipients: List[String], subject: String, body: String): String = {
+    val msgbody = new Body(new Content(body))
+    val rcpts = new Destination(recipients.asJava)
+    val message = new Message(new Content(subject), msgbody)
+    val request = new SendEmailRequest(from, rcpts, message)
+    val result = client.sendEmail(request)
+    println(result.toString)
+    result.getMessageId()
+  }
 }
--- src/main/scala/com/balihoo/fulfillment/workers/EmailWorker.scala
+++ src/main/scala/com/balihoo/fulfillment/workers/EmailWorker.scala
@@ -4,19 +4,70 @@ import com.balihoo.fulfillment.{SQSAdapter, SWFAdapter, SESAdapter}
 import com.amazonaws.services.simpleworkflow.model.ActivityTask
 import com.balihoo.fulfillment.config.PropertiesLoader
 
-class EmailWorker(swfAdapter: SWFAdapter, sqsAdapter: SQSAdapter. emailAdapter)
+class EmailWorker(swfAdapter: SWFAdapter, sqsAdapter: SQSAdapter, sesAdapter: SESAdapter)
   extends FulfillmentWorker(swfAdapter, sqsAdapter) {
 
   override def handleTask(task: ActivityTask) = {
-    completeTask(task.getTaskToken, "{\"-NOOP-\" : \"true\"}")
+    completeTask(task.getTaskToken, "{\"-EMAIL-\" : \"true\"}")
+  }
+
+  def verifyAddress(address: String) = {
+    sesAdapter.verifyEmailAddress(address)
+  }
+
+  def listVerifiedEmailAddresses(printToScreen: Boolean): Array[String] = {
+    Array()
+  }
+
+  def sendEmail(recipients: Array[String], body: String): Boolean = {
+    true
   }
 }
 
 object emailworker {
   def main(args: Array[String]) {
     val config = new PropertiesLoader(".emailworker.properties")
-    val emailworker = new EmailWorker(new SWFAdapter(config), new SQSAdapter(config), new EmailAdapter(config))
+    val emailworker = new EmailWorker(new SWFAdapter(config), new SQSAdapter(config), new SESAdapter(config))
     println("Running emailworker worker")
     emailworker.work()
+  }
+}
+
+object test_emailworker {
+  def main(args: Array[String]) {
+    val config = new PropertiesLoader(".emailworker.properties")
+    val emailworker = new EmailWorker(new SWFAdapter(config), new SQSAdapter(config), new SESAdapter(config))
+    var options = collection.mutable.Map[String, Tuple2[String, () => Unit]] ()
+
+    def usage() = {
+      println("Select a test:")
+      options.keys.foreach{ k => println(s"[$k] " + options(k)._1)}
+    }
+
+    def verifyAddress() = {
+      println("enter the address to verify")
+      val address = readLine("verifyemail> ")
+      emailworker.verifyAddress(address)
+    }
+
+    def listAddresses() = {
+      emailworker.listVerifiedEmailAddresses(true)
+    }
+
+    options("h") = ("Display Help", usage)
+    options("v") = ("Verify Email Address", verifyAddress)
+    options("l") = ("List Verified Email Addresses", listAddresses)
+    options("q") = ("Quit", () => println("bye"))
+
+    var choice = "h"
+    while (choice != "q") {
+      if (options.contains(choice)) {
+        options(choice)._2()
+      } else {
+        println("select a valid option. 'h' for help.")
+      }
+      choice  = readLine("emailtest> ")
+    }
+
   }
 }
