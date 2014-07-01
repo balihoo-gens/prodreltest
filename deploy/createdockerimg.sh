9062c2e9517b2b1a27b0dc05905310c7db86b2a0
gerry ens <gens@balihoo.com>
2014-07-01 15:40:51 -0600

    tested

--- deploy/createdockerimg.sh
+++ deploy/createdockerimg.sh
@@ -2,12 +2,15 @@
 CURDIR=$(cd $(dirname "$0"); pwd)
 TMPDIR=$CURDIR/../insalltmp
 rm -rf $TMPDIR
+sudo docker rmi balihoo/fulfillment
+sudo docker pull dockerfile/java
+
 mkdir -p $TMPDIR
 cp $CURDIR/../target/scala-2.10/fulfillment-assembly-1.0-SNAPSHOT.jar $TMPDIR
 cp $CURDIR/dockerinstall.script $TMPDIR/install.sh
 chmod a+x $TMPDIR/install.sh
-DOCKERCMD=sudo docker run -d -v $TMPDIR:/share dockerfile/java /share/install.sh
-CONTAINER=$("$DOCKERCMD")
+CONTAINER=$(sudo docker run -d -v $TMPDIR:/share dockerfile/java /share/install.sh)
+echo saviong container $CONTAINER
 sudo docker commit $CONTAINER balihoo/fulfillment
 
 
