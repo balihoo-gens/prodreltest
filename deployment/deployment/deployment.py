b7ca617a1eb47371c667fab73a31b34c228b3a90
gerry ens <gens@balihoo.com>
2014-10-06 15:07:37 -0600

    refactor deploy options, rename instances

--- deployment/deployment/deployment.py
+++ deployment/deployment/deployment.py
@@ -2,36 +2,35 @@ from splogger import Splogger
 from packager import Packager
 from cloudformer import CloudFormer
 from uploader import Uploader
-
+from collections import namedtuple
 import os
 import json
 import time
 
 class Deployment(object):
-    def __init__(self, log_filename, region, pyversion, veversion, dasheip, unattended=False):
-        self._dasheip = dasheip
-        self._veversion = veversion
-        self._pyversion = pyversion
-        self._region = region
-        self._unattended = unattended
+    #
+    Config = namedtuple('DeploymentConfig', [
+        "access_key",
+        "secret_key",
+        "region",
+        "s3bucket",
+        "ssh_keyname",
+        "worker_instance_type",
+        "dash_instance_type",
+        "min_instances",
+        "max_instances",
+        "access_ip_mask",
+        "web_port",
+        "pyversion",
+        "veversion",
+        "dasheip",
+        "env"
+    ])
+
+    def __init__(self, log_filename, cfg):
+        self._cfg = cfg
         self._log_filename = log_filename
         self.log = Splogger(self._log_filename, component="deployment")
-
-    def verify_env_var(self, var):
-        if var not in os.environ:
-            if self._unattended:
-                self.error(var + " not found in env.")
-                return False
-
-            print(var + " not found in env. Please enter it now")
-            os.environ[var] = raw_input(var + ": ")
-        return True
-
-    def check_aws_requirements(self):
-        if self.verify_env_var("AWS_ACCESS_KEY_ID"):
-            if self.verify_env_var("AWS_SECRET_ACCESS_KEY"):
-                return True
-        return False
 
     def package(self, rootdir):
         self.log.debug("packaging " + rootdir)
@@ -44,54 +43,53 @@ class Deployment(object):
         self.log.debug("uploading " + pkgpath)
         u = Uploader(
             s3bucket,
-            self._region,
-            os.environ["AWS_ACCESS_KEY_ID"],
-            os.environ["AWS_SECRET_ACCESS_KEY"]
+            self._cfg.region,
+            self._cfg.access_key,
+            self._cfg.secret_key,
         )
         return u.upload_dir(pkgpath)
 
     def create_stack(self, s3url, template_file, script_file):
-        if not self.check_aws_requirements():
-            raise Exception("AWS Credentials not in environment")
-
         template_data = None
         with open(template_file) as tf:
             #validate json here by loading it
             template_data = json.loads(tf.read())
 
-
-        access_key = os.environ["AWS_ACCESS_KEY_ID"]
-        secret_key = os.environ["AWS_SECRET_ACCESS_KEY"]
         self.log.debug("creating a stack using " + s3url)
-        c = CloudFormer(self._region, access_key, secret_key)
+        c = CloudFormer(self._region, self._cfg.access_key, self._cfg.secret_key)
+
+        dash_class = "com.balihoo.fulfillment.dashboard.dashboard"
+        cfg = self._cfg
 
         parameters = {
-            "KeyName" : "paul-ami-pair",
-            "WorkerInstanceType" : "m1.small",
-            "DashboardInstanceType" : "t1.micro",
-            "MinInstances" : "1",
-            "MaxInstances" : "10",
-            "WebPort" : "8080",
-            "WorkerScript" : self.gen_script(script_file, access_key, secret_key, s3url, None, ""),
-            "DashboardScript" : self.gen_script(script_file, access_key, secret_key, s3url, self._dasheip, "com.balihoo.fulfillment.dashboard.dashboard")
+            "KeyName"               : cfg.ssh_keyname,
+            "WorkerInstanceType"    : cfg.worker_instance_type,
+            "DashboardInstanceType" : cfg.dash_instance_type,
+            "MinInstances"          : cfg.min_instances,
+            "MaxInstances"          : cfg.max_instances,
+            "AccessIPMask"          : cfg.access_ip_mask,
+            "WebPort"               : cfg.web_port,
+            "WorkerScript"          : self.gen_script(script_file, s3url, None, ""),
+            "DashboardScript"       : self.gen_script(script_file, s3url, self._dasheip, dash_class),
+            "Environment"           : cfg.env,
         }
 
         #json dump guarantees valid json, but not a valid template per se
         stackname = "fulfillment%d" % (int(time.time()),)
         return c.create_stack(stackname, json.dumps(template_data), parameters)
 
-    def gen_script(self, script_file, access_key, secret_key, s3_bucket_url, eip, classes):
+    def gen_script(self, script_file, s3_bucket_url, eip, classes):
         pieces = [
             "#!/bin/bash",
             "set -e",
-            "AWSACCESSKEY=%s" % access_key,
-            "AWSSECRETKEY=%s" % secret_key,
-            "AWSREGION=%s" % self._region,
-            "S3BUCKETURL=%s" % s3_bucket_url,
-            "CLASSNAMES=%s" % classes,
-            "VEDIR=/opt/balihoo/virtualenv",
-            "PYVERSION=%s" % self._pyversion,
-            "VEVERSION=%s" % self._veversion,
+            "AWSACCESSKEY=%s" % self._cfg.access_key,
+            "AWSSECRETKEY=%s" % self._cfg.secret_key,
+            "AWSREGION=%s"    % self._cfg.region,
+            "S3BUCKETURL=%s"  % s3_bucket_url,
+            "CLASSNAMES=%s"   % classes,
+            "VEDIR=%s"        % "/opt/balihoo/virtualenv",
+            "PYVERSION=%s"    % self._cfg.pyversion,
+            "VEVERSION=%s"    % self._cfg.veversion,
         ]
 
         #optionally add the dashboard eip option.
