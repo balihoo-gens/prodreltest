941893ee2ab7310161d8e77686455c1b8a1e7c78
gerry ens <gens@balihoo.com>
2014-11-06 15:49:07 -0700

    comments

--- launcher/swfworker.py
+++ launcher/swfworker.py
@@ -16,14 +16,16 @@ class UnknownRegionException(Exception):
     pass
 
 """ container class for access to async swf task polling """
-PollInfo = namedtuple('PollInfo', ['thread', 'queue', 'event', 'stop', 'get'])
+PollInfo = namedtuple('PollInfo', ['stop', 'get'])
 
 """ wrapper for a SWF task. Attaches complete and fail functions """
 Task = namedtuple('Task', ['params', 'complete', 'fail'])
 
 class SwfWorker(swf.ActivityWorker):
-    """ """
+    """ Extension of the SWF Activity worker supporting asynchronous polling """
+
     def __init__(self, region_name, name, domain, version):
+        """ construct the worker, and register it """
         region = self.resolve_region(region_name)
         task_list = name + version
         super(SwfWorker, self).__init__(
@@ -36,6 +38,7 @@ class SwfWorker(swf.ActivityWorker):
         self.register()
 
     def resolve_region(self, region_name):
+        """ resolves a region object from a region name """
         region_list = boto.swf.regions()
         region_names = [r.name for r in region_list]
         regions = dict(zip(region_names, region_list))
@@ -45,6 +48,9 @@ class SwfWorker(swf.ActivityWorker):
             raise UnknownRegionException(region_name)
 
     def register(self):
+        """ register a new activity type for this worker
+        Ignore the exception if already registered
+        """
         at = swf.ActivityType(
             region=self.region,
             domain=self.domain,
@@ -58,30 +64,45 @@ class SwfWorker(swf.ActivityWorker):
             pass
 
     def get_task(self):
+        """ poll for a task
+        @returns the task if valid, None otherwise
+        """
         task = self.poll()
         if 'activityId' in task:
             return task
         return None
 
     def start_async_polling(self):
+        """ starts a thread to poll swf into a queue
+        task are wrapped into Task objects
+        @returns PollInfo object - contains methods to
+            get a task or stop the thread
+        """
         e = Event()
         q = queue.Queue()
+        complete = self.complete
+        fail = self.fail
 
         def _run():
+            """ function to run by the thread
+            closes over self.poll and constructs new Task
+            objects that close over self.fail and self.complete
+            """
             while not e.is_set():
                 task = self.poll()
                 if 'activityId' in task:
                     token = task['taskToken']
                     q.put(Task(
                         params=json.loads(task['input']),
-                        complete=lambda result=None: self.complete(token, result),
-                        fail=lambda details=None: self.fail(token, details),
+                        complete=lambda result=None: complete(token, result),
+                        fail=lambda details=None: fail(token, details),
                     ))
 
-        def _stop():
-            e.set()
-
         def _get ():
+            """ local function to return as part of the pollinfo
+            to get a task from the queue
+            @returns Task object or None
+            """
             try:
                 return q.get_nowait()
             except queue.Empty:
@@ -89,7 +110,7 @@ class SwfWorker(swf.ActivityWorker):
 
         t = Thread(target=_run)
         t. start()
-        return PollInfo(thread=t, queue=q, event=e, stop=_stop, get=_get)
+        return PollInfo(stop=e.set, get=_get)
 
 if __name__ == "__main__":
     w = SwfWorker(region_name="us-west-2", domain="fauxfillment", name="launcher", version="1")
